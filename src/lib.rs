cc7c38400c7eb0ff2ddab3083e170ad836099d70
hwipl <33433250+hwipl@users.noreply.github.com>
2020-01-25 14:23:18 +0100

    move symbol filtering into Symbols::new()
    Move the filtering of the symbols into Symbols::new() so that only
    filtered symbols are in the new Symbols.
    
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,32 @@
 use std::fs;
 
 pub struct Symbols {
-    symbols: String,
+    symbols: Vec<String>,
 }
 
 impl Symbols {
-    pub fn from_file(filename: &str) -> Symbols {
-        let symbols = fs::read_to_string(filename).unwrap();
-        Symbols { symbols }
+    fn from_file(filename: &str) -> String {
+        fs::read_to_string(filename).unwrap()
     }
 
     pub fn new() -> Symbols {
-        Symbols::from_file("/proc/kallsyms")
+        let mut symbols = Vec::new();
+        let contents = Symbols::from_file("/proc/kallsyms");
+
+        for line in contents.lines() {
+            let l: Vec<&str> = line.split_whitespace().collect();
+
+            if l[1] == "t" || l[1] == "T" {
+                symbols.push(String::from(l[2]));
+            }
+        }
+
+        Symbols{ symbols }
     }
 
     pub fn print(&self) {
-        for line in self.symbols.lines() {
-            let l: Vec<&str> = line.split_whitespace().collect();
-            if l[1] == "t" || l[1] == "T" {
-                println!("{}", l[2]);
-            }
+        for s in &self.symbols {
+            println!("{}", s);
         }
     }
 }
